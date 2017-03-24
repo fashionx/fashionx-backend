90c64e7ec16d4951ede4e9de7ece0c69e4bfa1fc
Unknown <appfashionx@gmail.com>
2017-03-25 01:18:38 +0200

    editting db

--- config.js
+++ config.js
@@ -3,4 +3,10 @@ module.exports = {
 
   'secret' : '',
   'database' : 'mongodb://admin:admin@ds129610.mlab.com:29610/fashionx-db'
+
+  'response' : {
+    'success' : Boolean,
+    'message' : String
+  }
+  
 };
