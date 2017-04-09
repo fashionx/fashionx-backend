6664f3c74514217fb43fa92cfc52b83d63da9505
Unknown <appfashionx@gmail.com>
2017-04-09 14:00:57 +0300

    add cluster, log viewer and coloring with chalk, worker control, create constants and edit www

--- config/db.js
+++ config/db.js
@@ -1,9 +1,23 @@
-var mongoose = require ('mongoose'); // get our config file
-//var config = require('./config');
+var mongoose = require ('mongoose');
+var connectionUri = 'mongodb://root:root@ds135680.mlab.com:35680/nodejs-jwt';
+var log = require('../utilities/log');
+var consoleLog = new log.console();
 
-//mongoose.connect(config.database);
+mongoose.connect(connectionUri, (err) => {
+    var message = 'DB Connection status: ' + !err;
+    if (!err) {
+        consoleLog.success(message);
+    }else {
+        consoleLog.error(message);
+    }
+});
 
-mongoose.connect('mongodb://root:root@ds135680.mlab.com:35680/nodejs-jwt');
+
+
+
+
+
+/*mongoose.connect('mongodb://root:root@ds135680.mlab.com:35680/nodejs-jwt');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
@@ -11,4 +25,4 @@ db.once('open', function callback() {
   console.log("Connection with database succeeded.");
 });
 
-exports.db = db;
+exports.db = db; */
---
+++ resources/constants.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const collectionNames = {
+  Customers : 'Customers',
+  Discounts : 'Discounts',
+  Locations : 'Locations',
+  ProductReservations : 'ProductReservations',
+  Products : 'Products',
+  UserLocations : 'UserLocations'
+  Users : 'Users',
+  Wardrops : 'Wardrops'
+}
+
+// 'll add other error codes'
+const errorCodes = {
+  missingToken : 'missing_token'
+  unableToAuthenticate : 'unable_to_authenticate',
+  unableToFindUser : 'unable_to_find_user'
+}
+
+module.exports = {
+  collectionNames : collectionNames,
+  errorCodes : errorCodes
+}
